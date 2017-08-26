baea8aa04b893dd44345effc51ae60726d7430e9
Stefan Ritter <stefan.ritter@sap.com>
2017-08-26 18:18:19 +0200

    use curl

--- server/utils/useragent.js
+++ server/utils/useragent.js
@@ -1,1 +1,2 @@
-module.exports = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36'; 
+//module.exports = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36';
+module.exports = 'curl/7.47.0';
