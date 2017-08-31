a5174a29e81656373fd01ba073a14d2cf003221b
Stefan Ritter <stefan.ritter@sap.com>
2017-08-31 07:23:16 +0200

    add options

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -5,11 +5,16 @@ var mongoose = require('mongoose');
 require('../models');
 
 module.exports = function (config) {
-  mongoose.connect(config.datastoreURI);
-  var db = mongoose.connection;
+    var options = {
+        server: {socketOptions: {keepAlive: 300000, connectTimeoutMS: 30000 }},
+        replset: {socketOptions: {keepAlive: 300000, connectTimeoutMS : 30000 }}
+    };
 
-  db.on('error', console.error.bind(console, 'db connection error...'));
-  db.once('open', function () {
-    console.log('db connection opened');
-  });
+    mongoose.connect(config.datastoreURI, options);
+
+    var db = mongoose.connection;
+    db.on('error', console.error.bind(console, 'db connection error...'));
+    db.once('open', function () {
+        console.log('db connection opened');
+    });
 };
